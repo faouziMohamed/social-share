8ca338f80c656489ce3078203a50131e1bb04d77
Faouzi Mohamed <57812398+faouziMohamed@users.noreply.github.com>
2021-10-23 02:40:28 +0100

    Webvital

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,10 @@
 import '../sass/global.scss';
 
+// export function reportWebVitals(metric) {
+//   // eslint-disable-next-line no-console
+//   console.log(metric);
+// }
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
