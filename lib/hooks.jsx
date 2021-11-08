1fd7b9a50f30ab05a0c98ae25801a958614af448
Faouzi Mohamed <57812398+faouziMohamed@users.noreply.github.com>
2021-11-09 00:29:37 +0100

    add new fields to the usr model, Add new packages

--- lib/hooks.jsx
+++ lib/hooks.jsx
@@ -1,15 +1,12 @@
 import useSWR from 'swr';
 
-export const fetcher = (url) => fetch(url).then((r) => r.json());
+export const fetcher = (url) => fetch(url).then((response) => response.json());
 
 export function useUser(username = '') {
-  const { data, mutate } = useSWR(`/api/user/${username || ''}`, fetcher);
-  // if data is not defined, the query has not completed
-  const loading = !data;
+  const { data, mutate } = useSWR(`/api/users/${username || ''}`, fetcher);
   const user = data?.user;
-  return [user, { mutate, loading }];
+  return [user, { mutate, loading: !data }];
 }
-
 export const useFetch = (url) => {
   const { data, error, mutate } = useSWR(url, fetcher);
   return [data, { loading: !data, error, mutate }];
